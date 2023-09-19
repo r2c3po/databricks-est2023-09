efa3ba504e1c1d7b92410f61c64395a4d8b56a0e
bolivarc@fordellconsulting.com <bolivarc@fordellconsulting.com>
2023-09-19 22:16:25 +0000

    WIP

--- ingest-cc-transactions.py
+++ ingest-cc-transactions.py
@@ -19,6 +19,8 @@ DeltaTable.createIfNotExists(spark) \
 # COMMAND ----------
 
 # MAGIC %sh
+# MAGIC ## TESTING copying files
+# MAGIC cd /dbfs/FileStore/shared_uploads/bolivarc@fordellconsulting.com
 # MAGIC ls -l /dbfs/FileStore/shared_uploads/bolivarc@fordellconsulting.com
 # MAGIC #cp /dbfs/FileStore/shared_uploads/bolivarc@fordellconsulting.com/accountactivity_eod_2023_09_17_download_2023_09_18.csv /dbfs/FileStore/shared_uploads/bolivarc@fordellconsulting.com/accountactivity_eod_2023_09_17_download_2023_09_18_2.csv
 # MAGIC
@@ -50,7 +52,12 @@ DeltaTable.createIfNotExists(spark) \
 # COMMAND ----------
 
 # MAGIC %sh
+# MAGIC # Reset CheckPoint
+# MAGIC #ls -ltr "/dbfs/FileStore/shared_uploads/bolivarc@fordellconsulting.com/checkpoint"
+# MAGIC rm -r "/dbfs/FileStore/shared_uploads/bolivarc@fordellconsulting.com/checkpoint"
 # MAGIC mkdir "/dbfs/FileStore/shared_uploads/bolivarc@fordellconsulting.com/checkpoint"
+# MAGIC ls -ltr "/dbfs/FileStore/shared_uploads/bolivarc@fordellconsulting.com/checkpoint"
+# MAGIC echo "CheckPoint Data removed"
 
 # COMMAND ----------
 
@@ -64,13 +71,7 @@ accountActivitySchema = StructType([    StructField("TransactionDt", DateType(),
                                         StructField("CreditAmt", FloatType(), True),
                                         StructField("BalanceAmt", FloatType(), False)])
 
-accountActivityStreamSchema = StructType([    StructField("FileName", StringType(), False),
-                                        StructField("FileTimestamp", TimestampType(), False),
-                                        StructField("TransactionDt", DateType(), False),
-                                        StructField("TransactionDescr", StringType(), False),
-                                        StructField("DebitAmt", FloatType(), True),
-                                        StructField("CreditAmt", FloatType(), True),
-                                        StructField("BalanceAmt", FloatType(), False)])
+
 #################################################
 # This is START of the the old batch code.
     # accountActivityDataFile = spark.read.load("dbfs:/FileStore/shared_uploads/bolivarc@fordellconsulting.com/accountactivity_eod_2023_09_17_download_2023_09_18_2.csv", format = "csv", schema = accountActivitySchema, header = False, dateFormat = "MM/dd/yyyy").select(col("_metadata.file_name").alias("FileName"),col("_metadata.file_modification_time").alias("FileTimestamp"),"*")
@@ -90,21 +91,49 @@ accountActivityStreamSchema = StructType([    StructField("FileName", StringType
 accountActivityFileFolder = "dbfs:/FileStore/shared_uploads/bolivarc@fordellconsulting.com"
 accountActivityCheckPointPath = "dbfs:/FileStore/shared_uploads/bolivarc@fordellconsulting.com/checkpoint"
 
-accountActivityFileStream = spark.readStream.schema(accountActivitySchema).csv(accountActivityFileFolder).select(col("_metadata.file_name").alias("FileName"),col("_metadata.file_modification_time").alias("FileTimestamp"),"TransactionDt","TransactionDescr","DebitAmt")
+
+#b4 using read.load syntax
+# accountActivityFileStream = spark.readStream.format("csv").schema(accountActivitySchema).option("date_format", "MM/dd/yyyy").load(accountActivityFileFolder). \
+#     select( \
+#         col("_metadata.file_name").alias("FileName"), \
+#         col("_metadata.file_modification_time").alias("FileTimestamp"), \
+#         "TransactionDt", \
+#         # to_date(col("TransactionDt"),"MM/dd/yyyy").alias("TransactionDt"), \
+#         "TransactionDescr", \
+#         "DebitAmt")
+    
+accountActivityFileStream = spark.readStream.load("dbfs:/FileStore/shared_uploads/bolivarc@fordellconsulting.com", format = "csv", schema = accountActivitySchema, header = False, dateFormat = "MM/dd/yyyy").select( \
+        col("_metadata.file_name").alias("FileName"), \
+        col("_metadata.file_modification_time").alias("FileTimestamp"), \
+        "TransactionDt", \
+        # to_date(col("TransactionDt"),"MM/dd/yyyy").alias("TransactionDt"), \
+        "TransactionDescr", \
+        "DebitAmt").filter("DebitAmt is not null")
 
 accountActivityDeltaStream = accountActivityFileStream.writeStream.format("delta").option("checkpointLocation", accountActivityCheckPointPath).toTable("StgCcDebitTransactions")
 
-TODO - format the date !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 
 
 
 # COMMAND ----------
 
 # MAGIC %sql
-# MAGIC select * from StgCcDebitTransactions order by transactiondt
+# MAGIC -- select count(*),transactiondt 
+# MAGIC --  from StgCcDebitTransactions 
+# MAGIC --  --where transactiondt is not null 
+# MAGIC -- group by transactiondt 
+# MAGIC -- order by transactiondt
+# MAGIC
+# MAGIC select *
+# MAGIC  from StgCcDebitTransactions  --where DebitAmt is null
+# MAGIC order by transactiondt
+# MAGIC
+# MAGIC
 
 # COMMAND ----------
 
+## STOP stream
 accountActivityDeltaStream.stop()
 
 # COMMAND ----------
@@ -123,11 +152,55 @@ DeltaTable.createIfNotExists(spark) \
 
 # COMMAND ----------
 
+# MAGIC %sh
+# MAGIC # Create location for checkpoints
+# MAGIC mkdir /Workspace/Shared/delta
+# MAGIC mkdir /Workspace/Shared/delta/CcDebitTransactions
+# MAGIC mkdir /Workspace/Shared/delta/CcDebitTransactions/_checkpoints
+
+# COMMAND ----------
+
+def mergeToCcDebitTransactions(microDf, atchId):
+    # Remember the micro batch and BatchId get automatically passed we just need to name them
+    (CcDebitTransactions.alias("t")
+     .merge(
+         microDf.alias("s"),
+         "s.TransactionDt = t.TransactionDt and s.TransactionDescr = t.TransactionDescr and s.DebitAmt = t.DebitAmt"
+     )
+     .whenNotMatchedInsertAll()
+     .execute
+     )
+
+
+# COMMAND ----------
+
 # Load from STG to "silver" table as a stream
 # Start Stream
-ccDebitTransactionsStreamIn = spark.readStream.format("delta").load("dbfs:/user/hive/warehouse/stgccdebittransactions")
+# ccDebitTransactionsStreamIn = spark.readStream.format("delta").load("dbfs:/user/hive/warehouse/stgccdebittransactions")
 
-ccDebitTransactionsDeltaStream = ccDebitTransactionsStreamIn.writeStream.format("delta").outputMode("append").option("checkpointLocation", "/tmp/delta/_checkpoints/").toTable("CcDebitTransactions")
+ccDebitTransactionsStreamIn = spark.readStream.format("delta") \
+    .option("withEventTimeOrder", "true") \
+    .table("stgccdebittransactions") \
+    .groupBy("TransactionDt", "TransactionDescr", "DebitAmt") \
+    .agg(count("*").alias("DupCount"))
+
+HOW DOES IT KNOW THE COLUMN TO USE FOR EventTimeOrder???
+# .withWatermark("TransactionDt", "5 days")
+
+# using update instead of append
+# we want to have the most up to date daily transactions, but we know that there are delays in transactions being posted
+# assuming we can have revisions (delays in posting transactions up to 5 days)
+ccDebitTransactionsDeltaStream = ccDebitTransactionsStreamIn.writeStream.format("delta") \
+    .outputMode("append") \
+    .option("checkpointLocation", "/Workspace/Shared/delta/CcDebitTransactions/_checkpoints/") \
+    .toTable("CcDebitTransactions") \
+    .foreachBatch(mergeToCcDebitTransactions)
+
+
+# COMMAND ----------
+
+
+
 
 
 # COMMAND ----------
